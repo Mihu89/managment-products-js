a628f806fb4f0124ff1f8459bd8f40d5cb56f6c3
Mihail <mcoscodan@ranorex.com>
2024-01-15 18:39:05 +0200

    Add event listner for save products into list

--- App.js
+++ App.js
@@ -48,13 +48,38 @@ class UI {
         // remove html node after 3 seconds
         setTimeout(function(){
                 document.querySelector(".alert").remove();
-        }, 23000);
+        }, 4000);
     }
 }
 
-var product = new Product("Alune", "Alune de padure", 80, 2024);
-var ui = new UI();
-ui.addProduct(product);
-var el = document.getElementsByName("delete")[0];
-ui.deleteProduct(el);
-ui.showMessage("It really works", "danger");
+// DOM Events
+document.getElementById("product-form")
+.addEventListener("submit", function(e){
+
+    let productName = document.getElementById("name").value;
+    let productDescription = document.getElementById("description").value;
+    let productPrice = document.getElementById("price").value;
+    let productYear = document.getElementById("year").value;
+
+    // create product object from form values
+    const product = new Product(productName, productDescription, productPrice, productYear);
+    const ui = new UI();
+
+    // validate user input
+    if(productName ==="" || productDescription ===""  
+    || (productPrice ==="" || parseInt(productPrice)< 0) 
+    || (productYear ==="" || parseInt(productYear)< 1800)){
+        ui.showMessage("Please insert data in all fields", "danger");
+    } else{
+        // save product
+    ui.addProduct(product);
+    ui.showMessage("Product added Successifull", "success");
+    // clear form data
+    ui.resetForm();
+    e.preventDefault();
+    }
+});
+
+// event listener for delete product from the list
+
+
